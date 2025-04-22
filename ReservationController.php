c7fbe67f66384313bf040510079bd568a8d7d617
dounia-halit <Dounia_Halit@etu.u-bourgogne.fr>
2025-04-22 15:48:24 -0400

    Update and rename controllers/test.txt to ReservationController.php

---
+++ ReservationController.php
@@ -0,0 +1,15 @@
+
+<?php
+
+class ReservationController
+{
+    public function home()
+    {
+        require_once 'views/home.php';
+    }
+
+    public function makeReservation()
+    {
+        require_once 'views/reservation_form.php';
+    }
+}
