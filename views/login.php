950d05dbf252aeb1ca3d9f150a3b2bb280f60a78
dounia-halit <Dounia_Halit@etu.u-bourgogne.fr>
2025-04-22 15:51:12 -0400

    Create login.php

---
+++ views/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form method="POST" action="?page=login">
+        <label for="email">Email :</label>
+        <input type="email" name="email" required><br><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>
