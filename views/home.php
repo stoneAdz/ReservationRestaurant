9295a754fad7eda16b14bd51966faf15ff6a2f56
dounia-halit <Dounia_Halit@etu.u-bourgogne.fr>
2025-04-22 15:50:38 -0400

    Update and rename test.txt to home.php

---
+++ views/home.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil - Réservation Restaurant</title>
+</head>
+<body>
+    <h1>Bienvenue sur le système de réservation</h1>
+    <p><a href="?page=reserve">Faire une réservation</a></p>
+    <p><a href="?page=login">Se connecter</a></p>
+</body>
+</html>
