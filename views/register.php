f976e3999a11dd2f647d318c5d637b3786f2ac97
dounia-halit <Dounia_Halit@etu.u-bourgogne.fr>
2025-04-22 16:27:12 -0400

    Create register.php

---
+++ views/register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Inscription</title>
+</head>
+<body>
+    <h1>Inscription</h1>
+    <form method="POST" action="?page=register">
+        <label for="name">Nom :</label>
+        <input type="text" name="name" required><br><br>
+
+        <label for="email">Email :</label>
+        <input type="email" name="email" required><br><br>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">S'inscrire</button>
+    </form>
+
+    <p>Déjà un compte ? <a href="?page=login">Se connecter</a></p>
+</body>
+</html>
