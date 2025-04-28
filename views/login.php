6eeb6013acd46c379ca0125d7e91488531a13cd9
Sairi Samir <sairisamir23@gmail.com>
2025-04-28 23:39:11 +0200

    Update login.php

--- views/login.php
+++ views/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="assets/css/login.css"> <!-- Lien vers ton fichier CSS -->
+    <link rel="stylesheet" href="assets/css/login.css"> 
 </head>
 <body class="login-container">
 
